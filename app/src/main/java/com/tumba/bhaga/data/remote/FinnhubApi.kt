e5fc72724a554d90adce14fe127ccf8e22c91d91
moris071651 <moris071651@proton.me>
2025-08-28 10:52:38 +0300

    added news api entry

--- app/src/main/java/com/tumba/bhaga/data/remote/FinnhubApi.kt
+++ app/src/main/java/com/tumba/bhaga/data/remote/FinnhubApi.kt
@@ -1,26 +1,42 @@
 package com.tumba.bhaga.data.remote
 
+import com.tumba.bhaga.data.remote.dto.CompanyNewsDto
 import com.tumba.bhaga.data.remote.dto.CompanyProfileDto
 import com.tumba.bhaga.data.remote.dto.QuoteDto
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.request.get
 import io.ktor.client.request.parameter
+import java.time.LocalDate
 
 class FinnhubApi(
     private val client: HttpClient,
     private val apiKey: String
 ) {
+
+    private val API_PREFIX = "https://finnhub.io/api/v1"
     suspend fun getQuote(ticker: String): QuoteDto {
-        return client.get("https://finnhub.io/api/v1/quote") {
+        return client.get("$API_PREFIX/quote") {
             parameter("symbol", ticker)
             parameter("token", apiKey)
         }.body()
     }
 
     suspend fun getCompanyProfile(ticker: String): CompanyProfileDto {
-        return client.get("https://finnhub.io/api/v1/stock/profile2") {
+        return client.get("$API_PREFIX/stock/profile2") {
             parameter("symbol", ticker)
+            parameter("token", apiKey)
+        }.body()
+    }
+
+    suspend fun getCompanyNews(ticker: String): CompanyNewsDto {
+        val today = LocalDate.now()
+        val dayAgo = today.minusDays(1)
+
+        return client.get("$API_PREFIX/company-news") {
+            parameter("symbol", ticker)
+            parameter("from", dayAgo.toString())
+            parameter("to", today.toString())
             parameter("token", apiKey)
         }.body()
     }
