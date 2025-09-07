0215840507a44ccdef6b9c6c4a13a2662fb399f6
moris071651 <moris071651@proton.me>
2025-09-07 19:17:13 +0300

    fav screen updated

--- app/src/main/java/com/tumba/bhaga/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/tumba/bhaga/ui/screens/favourites/FavouritesScreen.kt
@@ -1,23 +1,21 @@
 package com.tumba.bhaga.ui.screens.favourites
 
-import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
 import com.tumba.bhaga.ui.components.StockSummaryList
 
 @Composable
 fun FavouritesScreen(
     onStockClick: (String) -> Unit,
-    viewModel: FavouritesViewModel = FavouritesViewModel()
+    viewModel: FavouritesViewModel = hiltViewModel()
 ) {
     val favourites by viewModel.favourites.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
--- app/src/main/java/com/tumba/bhaga/ui/screens/favourites/FavouritesViewModel.kt
+++ app/src/main/java/com/tumba/bhaga/ui/screens/favourites/FavouritesViewModel.kt
@@ -1,15 +1,19 @@
 package com.tumba.bhaga.ui.screens.favourites
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.tumba.bhaga.data.di.AppModule.repository
+import com.tumba.bhaga.data.repository.FavouritesRepository
 import com.tumba.bhaga.domain.models.StockSummary
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import javax.inject.Inject
 
-class FavouritesViewModel() : ViewModel() {
+@HiltViewModel
+class FavouritesViewModel @Inject constructor(
+    private val repository: FavouritesRepository
+) : ViewModel() {
 
     private val _favourites = MutableStateFlow<List<StockSummary>>(emptyList())
     val favourites: StateFlow<List<StockSummary>> = _favourites
