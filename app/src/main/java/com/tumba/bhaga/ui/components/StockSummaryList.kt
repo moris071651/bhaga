07a77391b212286ff414dd68a047a2ab568621ae
moris071651 <moris071651@proton.me>
2025-09-09 17:54:48 +0300

    fixing summary card

--- app/src/main/java/com/tumba/bhaga/ui/components/StockSummaryList.kt
+++ app/src/main/java/com/tumba/bhaga/ui/components/StockSummaryList.kt
@@ -1,10 +1,14 @@
 package com.tumba.bhaga.ui.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.tumba.bhaga.domain.models.StockSummary
 
 @Composable
@@ -14,10 +18,17 @@ fun StockSummaryList(
     modifier: Modifier = Modifier
 ) {
     LazyColumn(
-        modifier = modifier.fillMaxHeight()
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        contentPadding = PaddingValues(vertical = 8.dp),
+        modifier = modifier.fillMaxSize()
     ) {
-        items(stocks, key = {it.ticker}) { s ->
-            StockSummaryCard(s, onClick = { onStockClick(s.ticker) })
+        items(stocks, key = { it.ticker }) { stock ->
+            StockSummaryCard(
+                stock = stock,
+                onClick = {
+                    onStockClick(stock.ticker)
+                }
+            )
         }
     }
 }
