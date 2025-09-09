52d7e94172465b01d28e7f4e28a663e76fc8208d
moris071651 <moris071651@proton.me>
2025-09-09 17:59:24 +0300

    update settings screen

--- app/src/main/java/com/tumba/bhaga/ui/screens/search/SearchScreen.kt
+++ app/src/main/java/com/tumba/bhaga/ui/screens/search/SearchScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
@@ -21,6 +22,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -30,6 +32,7 @@ import androidx.navigation.NavHostController
 import com.tumba.bhaga.domain.models.SearchEntry
 import com.tumba.bhaga.ui.components.SearchStockList
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 import org.apache.commons.text.similarity.LevenshteinDistance
 import kotlin.math.min
 
@@ -39,14 +42,18 @@ fun SearchScreen(
     modifier: Modifier = Modifier,
     viewModel: SearchViewModel = hiltViewModel(),
 ) {
+    val listState = rememberLazyListState()
     val filtered = remember { mutableStateListOf<SearchEntry>() }
     var query by remember { mutableStateOf("") }
-    val entries by viewModel.entries.collectAsState()
 
     Column(
         modifier = modifier
             .fillMaxSize()
-            .padding(16.dp)
+            .padding(
+                top = 8.dp,
+                start = 8.dp,
+                end = 8.dp
+            )
     ) {
         OutlinedTextField(
             value = query,
@@ -55,34 +62,19 @@ fun SearchScreen(
             singleLine = true,
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 16.dp)
+                .padding(bottom = 8.dp)
         )
 
         LaunchedEffect(query) {
-            delay(500)
+            delay(1000)
             filtered.clear()
-            filtered.addAll(onSearch(query, entries))
-        }
-
-        SearchStockList(filtered, onStockClick)
-    }
-}
-
-private fun onSearch(query: String, entries: List<SearchEntry>): List<SearchEntry> {
-    if (query.isEmpty()) {
-        return listOf()
-    }
-
-    val ld = LevenshteinDistance.getDefaultInstance()
-
-    return entries
-        .map {
-            it to min(
-                ld.apply(it.ticker, query),
-                ld.apply(it.companyName, query)
+            filtered.addAll(
+                viewModel.getFilteredEntries(query)
             )
+
+            listState.animateScrollToItem(0)
         }
-        .sortedBy { it.second }
-        .take(10)
-        .map { it.first }
+
+        SearchStockList(filtered, onStockClick, listState)
+    }
 }
--- app/src/main/java/com/tumba/bhaga/ui/screens/search/SearchViewModel.kt
+++ app/src/main/java/com/tumba/bhaga/ui/screens/search/SearchViewModel.kt
@@ -8,13 +8,17 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
+import org.apache.commons.text.similarity.LevenshteinDistance
 import javax.inject.Inject
+import kotlin.collections.map
+import kotlin.math.min
 
 @HiltViewModel
 class SearchViewModel @Inject constructor(
     private val repository: SearchRepository
 ) : ViewModel() {
-    private val _entries = MutableStateFlow<List<SearchEntry>>(listOf())
+
+    private val _entries = MutableStateFlow<List<SearchEntry>>(emptyList())
     val entries: StateFlow<List<SearchEntry>> = _entries
 
     init {
@@ -25,5 +29,28 @@ class SearchViewModel @Inject constructor(
         viewModelScope.launch {
             _entries.value = repository.getAllSearchEntries()
         }
+    }
+
+    fun getFilteredEntries(query: String): List<SearchEntry> {
+        val currentEntries = _entries.value
+        if (currentEntries.isEmpty() || query.isEmpty()) return emptyList()
+
+        if (query.isEmpty()) {
+            return listOf()
+        }
+
+        val q = query.lowercase()
+        val ld = LevenshteinDistance.getDefaultInstance()
+
+        return currentEntries
+            .map {
+                it to min(
+                    ld.apply(it.ticker.lowercase(), q),
+                    ld.apply(it.companyName.lowercase(), q)
+                )
+            }
+            .sortedBy { it.second }
+            .take(15)
+            .map { it.first }
     }
 }
