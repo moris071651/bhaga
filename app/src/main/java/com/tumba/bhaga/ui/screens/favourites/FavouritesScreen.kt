0fd52d247549817b209b275576bf46c9865ae011
moris071651 <moris071651@proton.me>
2025-09-09 17:55:30 +0300

    updating the screens

--- app/src/main/java/com/tumba/bhaga/ui/screens/favourites/FavouritesScreen.kt
+++ app/src/main/java/com/tumba/bhaga/ui/screens/favourites/FavouritesScreen.kt
@@ -1,24 +1,35 @@
 package com.tumba.bhaga.ui.screens.favourites
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tumba.bhaga.ui.components.StockSummaryList
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FavouritesScreen(
     onStockClick: (String) -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior,
     viewModel: FavouritesViewModel = hiltViewModel()
 ) {
     val favourites by viewModel.favourites.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
+
+    Log.d("debug", favourites.toString())
 
     when {
         isLoading -> {
@@ -38,7 +49,17 @@ fun FavouritesScreen(
             }
         }
         else -> {
-            StockSummaryList(favourites, onStockClick)
+            Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 8.dp)
+            ) {
+                StockSummaryList(
+                    favourites,
+                    onStockClick,
+                    Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/tumba/bhaga/ui/screens/home/HomeScreen.kt
+++ app/src/main/java/com/tumba/bhaga/ui/screens/home/HomeScreen.kt
@@ -1,23 +1,31 @@
 package com.tumba.bhaga.ui.screens.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.tumba.bhaga.ui.components.StockSummaryList
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     onStockClick: (String) -> Unit,
+    scrollBehavior: TopAppBarScrollBehavior,
     viewModel: HomeViewModel = hiltViewModel()
 ) {
     val stocks = viewModel.stocks
 
     if (stocks.isEmpty()) {
-        // Loading state
         Box(
             modifier = Modifier.fillMaxSize(),
             contentAlignment = Alignment.Center
@@ -25,6 +33,16 @@ fun HomeScreen(
             CircularProgressIndicator()
         }
     } else {
-        StockSummaryList(stocks, onStockClick)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(horizontal = 8.dp)
+        ) {
+            StockSummaryList(
+                stocks,
+                onStockClick,
+                Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
+            )
+        }
     }
 }
